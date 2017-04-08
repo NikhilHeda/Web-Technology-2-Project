1d4724cf764470f822a3c7ae9ce8ffd07b7ffb13
NikhilHeda <nikhilheda96@gmail.com>
2017-04-08 18:07:52 +0530

    Added Comet (Server-Sent Events) functionality for updating posts asynchronously

---
+++ php/posts_updates.php
@@ -0,0 +1,48 @@
+<?php
+	require "core.inc.php";
+	
+	header('Content-type: text/event-stream');
+
+	ob_flush();
+	flush();
+	
+	require 'connect.inc.php';
+	
+	$query = "
+		SELECT *
+		FROM `listens`
+		ORDER BY `listened_at` DESC;
+	";
+	
+	$old = 0;
+	if (isset($_SESSION['last_post']))
+		$old = $_SESSION['last_post'];
+	
+	while (True) {
+		
+		($query_res = mysql_query($query)) or (die(mysql_error()));
+		
+		$new = mysql_num_rows($query_res);
+		
+		if ($new > $old) {
+			$i = 0;
+			while ( ($row = mysql_fetch_assoc($query_res)) && ($i < $new - $old) ) {
+				echo "data: ". json_encode($row) . "\n";
+				echo "event: newpost\n\n";
+				
+				ob_flush();
+				flush();
+				
+				$i++;
+			}
+			
+			$old = $new;
+			$_SESSION['last_post'] = $old;
+		}
+		
+		sleep(5);
+	}
+	
+	unset($_SESSION['last_post']);
+
+?>
